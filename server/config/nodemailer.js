6d780421b1942c0a954506496b81b0c052f5f25e
Bruno Azevedo Chagas <bachagas@ig.com.br>
2017-09-18 09:23:29 -0700

    Fixed email configurations

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -4,6 +4,6 @@ exports.transport = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
         user: 'agroupware@gmail.com',
-        pass: '@ll1non3'
+        pass: '********'
     }
 });
